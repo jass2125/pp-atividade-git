a22d9bb40bcf1f391f3c5a1572ecdd3452b1db3a
Anderson Souza <jair_anderson_bs@hotmail.com>
2015-12-11 10:19:03 -0300

    Dao de usuario finalizado

--- src/main/java/io/github/jass2125/UsuarioDao.java
+++ src/main/java/io/github/jass2125/UsuarioDao.java
@@ -1,10 +1,29 @@
 package io.github.jass2125;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author Anderson Souza
  */
 public class UsuarioDao {
     
-    jdbc:derby://localhost:1527/pp
+    public void add(Usuario usuario) throws SQLException{ 
+        String url = "jdbc:derby://localhost:1527/pp";
+        String user = "pp";
+        String password = "123";
+        Connection connection = DriverManager.getConnection(url, user, password);
+        String sql = "insert into usuario(nome, email, fone) values(?, ?, ?);";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setString(1, usuario.getEmail());
+        ps.setString(2, usuario.getEmail());
+        ps.setString(3, usuario.getFone());
+        ps.execute();
+        
+    }
+    
+    
 }
