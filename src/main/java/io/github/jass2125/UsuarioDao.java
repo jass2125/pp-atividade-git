edac2ee6fa3021c2d6cd97e9c67c562f775ecc00
Anderson Souza <jair_anderson_bs@hotmail.com>
2015-12-11 16:18:15 -0300

    Correção na adição de usuario

--- src/main/java/io/github/jass2125/UsuarioDao.java
+++ src/main/java/io/github/jass2125/UsuarioDao.java
@@ -17,14 +17,13 @@ public class UsuarioDao implements UsuarioDaoIF {
         String user = "pp";
         String password = "123";
         Connection connection = DriverManager.getConnection(url, user, password);
-        String sql = "insert into PP.USUARIO(nome, email, senha, fone) values(?, ?, ?, ?);";
+        String sql = "insert into PP.USUARIO(nome, email, senha, fone) values(?, ?, ?, ?)";
         PreparedStatement ps = connection.prepareStatement(sql);
         ps.setString(1, usuario.getNome());
         ps.setString(2, usuario.getEmail());
         ps.setString(3, usuario.getPassword());
         ps.setString(4, usuario.getFone());
         ps.execute();
-        
     }
     
     public boolean login(String login, String senha) throws SQLException {
